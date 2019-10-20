583d1b66fd5bbf95c0be13dd74cd1a92f234cc67
Nihal <nihal9ns@gmail.com>
2019-10-20 19:07:09 +0530

    test

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
-const redis = require("redis");
-const client = redis.createClient({ url: "redis://localhost:6379" });
+const redis = require('redis');
+const client = redis.createClient({ url: 'redis://localhost:6379' });
 
-client.on("error", function (err) {
-  console.log("Error " + err);
+client.on('error', function(err) {
+  console.log('Error ' + err);
 });
 
-// with the previously create client
-client.set("visits", 1, function (err) {
+client.set('visits', 1, function(err) {
   if (err) {
-    throw err; /* in production, handle errors more gracefully */
+    throw err;
   } else {
-    client.get("visits", function (err, value) {
+    client.get('visits', function(err, value) {
       if (err) {
         throw err;
       } else {
